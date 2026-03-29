771b23c27eb5b8e05ce8afac5ec2e36acfaeb734
Zhakhongir <johongir260908@gmail.com>
2026-03-29 15:41:26 +0600

    first commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+
+
+int main() {
+    setlocale(LC_ALL, "RU");
+
+    std::cout << "All is okay!\n" << std::endl;
+    std::cout << "How are you?" << std::endl;
+    std::cout << "Привет" << std::endl;
+
+    return 0;
+}
