e232a8f9ccf0653cdc0bc324ab405ed0c80a7b72
Zhakhongir <johongir260908@gmail.com>
2026-04-17 10:10:27 +0600

    17_seventeenth_lesson

--- lessons/17_seventeenth_lesson.cpp
+++ lessons/17_seventeenth_lesson.cpp
@@ -1,5 +1,7 @@
 // Создание классов и объектов
 
+// Конструкторы, деструкторы и указатель this
+
 
 #include <iostream>
 #include <string>
@@ -14,13 +16,24 @@ private:
     string type;
 
 public:
+    Building(int y, string t) {
+        set_data(y, t);
+    }
+
+    Building() {
+    }
+
     void set_data(int y, string t) {
-        year = y;
+        this->year = y;
         type = t;
     }
 
     void get_info() {
         cout << "Type: " << type << ". Year: " << year << endl;
+    }
+
+    ~Building() {
+        cout << "Delete object!" << endl;
     }
 };
 
@@ -28,17 +41,17 @@ public:
 int main() {
     setlocale(LC_ALL, "RU");
 
-    Building school;
+    Building school(1998, "Школа");
     // school.type = "Школа";
     // school.year = 2000;
-    school.set_data(2000, "Школа");
+    // school.set_data(2000, "Школа");
     school.get_info();
 
 
-    Building house;
+    Building house(2003, "House");
     // house.type = "Дом 1";
     // house.year = 1997;
-    house.set_data(2010, "Дом 1");
+    // house.set_data(2010, "Дом 1");
     house.get_info();
 
     return 0;
